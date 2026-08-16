a5ed4767f1fecc5723193cef6fcdcf2be3fb4809
PADMORE YEBOAH <95287415+MAD-MORE@users.noreply.github.com>
2026-08-16 10:32:33 +0000

    Step 14: define official UCC source catalogue

---
+++ aski-backend/app/ucc_sources.py
@@ -0,0 +1,4 @@
+UCC_SOURCES = [
+    {"name": "UCC Homepage", "url": "https://ucc.edu.gh/", "type": "official"},
+    {"name": "UCC Admissions", "url": "https://admission.ucc.edu.gh/", "type": "official"},
+]
